74d9a116b7e245f67c6cc724e0d525b40a7993e2
Nutnicha Rachprasit <nrbaboo@hotmail.com>
2018-06-05 14:18:23 +0700

    add Correlation matching & open folder

--- dialog.cpp
+++ dialog.cpp
@@ -1,6 +1,10 @@
 #include "dialog.h"
 #include "ui_dialog.h"
 #include <QMessageBox>
+#include <Qtcore>
+#include <QtGui>
+#include <QFileDialog>
+
 using namespace cv;
 
 inline QImage  cvMatToQImage( const cv::Mat &inMat )
@@ -78,26 +82,118 @@ Dialog::~Dialog()
 
 void Dialog::on_btn_load_clicked()
 {
-    image = imread("D://Internship/MatchingImage/Picture/1.jpg", 1);
-    updateImage1();
-    chkImage1 = 1;
+    QString filename = QFileDialog::getOpenFileName(
+                this,
+                tr("Open File"),
+                "D://Internship/MatchingImage/Picture",                        //---------** only C:// or D://
+                "All file (*.*);;JPEG(*.jpg);;Bitmap(*.bmp);;PNG(*.png)"
+    );
+   std::string file1 = filename.toStdString();
+   image = imread(file1, 1);
+   updateImage1();
+   chkImage1 = 1;
+
 }
 
 void Dialog::on_btn_load2_clicked()
 {
-    image2 = imread("D://Internship/MatchingImage/Picture/3.jpg", 1);
+
     if(ui->chk_newImg->isChecked())
     {
-         updateImage2();
-         chkImage2 = 1;
+        QString filename = QFileDialog::getOpenFileName(
+                    this,
+                    tr("Open File"),
+                    "D://Internship/MatchingImage/Picture",                        //---------** only C:// or D://
+                    "All file (*.*);;JPEG(*.jpg);;Bitmap(*.bmp);;PNG(*.png)"
+        );
+       std::string file2 = filename.toStdString();
+       image2 = imread(file2, 1);
+       updateImage2();
+       chkImage2 = 1;
     }
 }
 
+
+int match_method;
+int max_Trackbar = 5;
+
+void Dialog::correlation()
+{
+    /* Mat resMorMat;
+     const int nRows = image.rows;
+     const int nCols = image.cols;
+     CvPoint *pPoint = new CvPoint[nRows*nCols];
+     pPoint[0] = cvPoint(100, 50);
+     resMorMat = image.clone();
+     circle(resMorMat, pPoint[0], 5, Scalar(255, 0, 0),1,8,0);
+
+     QPixmap imgIn = cvMatToQPixmap(resMorMat);
+     ui->label->setPixmap(imgIn);
+     ui->label->setScaledContents(true);
+     ui->label->show();*/
+
+     img = image.clone();
+     templ = image2.clone();
+
+
+     //namedWindow( image_window, WINDOW_AUTOSIZE );
+     //namedWindow( result_window, WINDOW_AUTOSIZE );
+     //const char* trackbar_label = "Method: \n 0: SQDIFF \n 1: SQDIFF NORMED \n 2: TM CCORR \n 3: TM CCORR NORMED \n 4: TM COEFF \n 5: TM COEFF NORMED";
+     //createTrackbar( trackbar_label, image_window, &match_method, max_Trackbar,MatchingMethod());
+     MatchingMethod( 0, 0 );
+
+     cv::Mat corr;
+     cv::matchTemplate(image, image2, corr, cv::TM_CCORR_NORMED);
+     correl = corr.at<float>(0,0);  // corr only has one pixel
+}
+void Dialog::MatchingMethod( int, void* )
+{
+      Mat img_display;
+      img.copyTo( img_display );
+      int result_cols =  img.cols - templ.cols + 1;
+      int result_rows = img.rows - templ.rows + 1;
+      result.create( result_rows, result_cols, CV_32FC1 );
+      bool method_accepts_mask = (CV_TM_SQDIFF == match_method || match_method == CV_TM_CCORR_NORMED);
+      if (use_mask && method_accepts_mask)
+      {
+          matchTemplate( img, templ, result, match_method, mask);
+      }
+      else
+        {
+          matchTemplate( img, templ, result, match_method);
+      }
+      normalize( result, result, 0, 1, NORM_MINMAX, -1, Mat() );
+      double minVal; double maxVal; Point minLoc; Point maxLoc;
+      Point matchLoc;
+      minMaxLoc( result, &minVal, &maxVal, &minLoc, &maxLoc, Mat() );
+      if( match_method  == TM_SQDIFF || match_method == TM_SQDIFF_NORMED )
+      {
+          matchLoc = minLoc;
+      }
+      else
+      {
+          matchLoc = maxLoc;
+      }
+      rectangle( img_display, matchLoc, Point( matchLoc.x + templ.cols , matchLoc.y + templ.rows ), Scalar(255, 0 ,0), 2, 8, 0 );
+
+
+      QPixmap imgIn = cvMatToQPixmap(img_display);
+      ui->label->setPixmap(imgIn);
+      ui->label->setScaledContents(true);
+      ui->label->show();
+
+      //rectangle( result, matchLoc, Point( matchLoc.x + templ.cols , matchLoc.y + templ.rows ), Scalar::all(0), 2, 8, 0 );
+      //imshow( image_window, img_display );
+      //imshow( result_window, result );
+      return;
+}
 void Dialog::on_btn_match_clicked()
 {
+
+
     if(chkImage1==0&&chkImage2==0)
     {
-        QMessageBox::information(this,tr("Can not matching!"),tr("Please select image and sample image."));
+        QMessageBox::information(this,tr("Can not matching!"),tr("Please select input image and sample image."));
         return;
     }
     else if(chkImage2==0)
@@ -107,12 +203,38 @@ void Dialog::on_btn_match_clicked()
     }
     else if(chkImage1==0)
     {
-        QMessageBox::information(this,tr("Can not matching!"),tr("Please select image."));
+        QMessageBox::information(this,tr("Can not matching!"),tr("Please select input image."));
         return;
     }
 
+    ui->result->clear();
+    updateImage1();
+
     if(ui->chk_shape->isChecked())
     {
-            QMessageBox::information(this,tr("matching!"),tr("Bla Bla Bla Bla Bla Bla."));
+        QMessageBox::information(this,tr("matching!"),tr("Bla Bla Bla Bla Bla Bla."));
     }
+    else if(ui->chk_correlation->isChecked())
+    {
+
+        correlation();
+        QString qstr = QString::number(correl);
+
+        ui->result->setColumnCount(2);
+        AddRoot("Correlation",qstr);
+    }
+}
+void Dialog::AddRoot(QString name,QString description)
+{
+    QTreeWidgetItem *itm= new QTreeWidgetItem(ui->result);
+    itm->setText(0,name);
+    itm->setText(1,description);
+    ui->result->addTopLevelItem(itm);
+}
+void Dialog::AddChild(QTreeWidgetItem *parent,QString name,QString description)
+{
+    QTreeWidgetItem *itm= new QTreeWidgetItem(ui->result);
+    itm->setText(0,name);
+    itm->setText(1,description);
+    parent->addChild(itm);
 }
--- dialog.h
+++ dialog.h
@@ -1,7 +1,7 @@
 #ifndef DIALOG_H
 #define DIALOG_H
 
-#include <QDialog>
+
 
 #include <QDialog>
 //--------------------------------------------- QT
@@ -9,12 +9,17 @@
 #include <QDir>
 #include <QString>
 #include <math.h>
+#include <Qtcore>
+#include <QtGui>
+#include <QTreeWidgetItem>
 
 //--------------------------------------------- OpenCV
+#include <opencv2/core/core.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include "opencv2/imgcodecs.hpp"
+
 #include <iostream>
 
 namespace Ui {
@@ -25,8 +30,12 @@ class Dialog : public QDialog
 {
     Q_OBJECT
 
+    void AddRoot(QString name,QString description);
+    void AddChild(QTreeWidgetItem *parent,QString name,QString description);
+
 public:
     explicit Dialog(QWidget *parent = 0);
+
     ~Dialog();
 
 private slots:
@@ -39,10 +48,22 @@ private slots:
 private:
     Ui::Dialog *ui;
 
+    void MatchingMethod( int, void* );
     void updateImage1();
     void updateImage2();
+    void correlation();
+
+
     cv::Mat image,image2;
     int chkImage1=0,chkImage2=0;
+    double correl,flagMatch=0;
+
+    bool use_mask;
+    cv::Mat img,templ,mask,result;
+    const char* image_window = "Source Image";
+    const char* result_window = "Result window";
+    const std::string file1,file2;
+
 };
 
 #endif // DIALOG_H
